024bbd999565e7d5964417fe75e003def26839b9
spoonty <sergey.kachalov.02@gmail.com>
2022-04-24 13:54:04 +0700

    Design of the application completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Title from "./components/title/title";
+import GameContainer from "./components/gameContainer/gameContainer";
+
+function App() {
+    return (
+        <div className="App">
+            <Title title="pokemon game" />
+            <div className="content">
+                <GameContainer />
+            </div>
+            <Title title="created by Sergey Kachalov" />
+        </div>
+    );
+}
+
+export default App;

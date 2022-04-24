2351244bf5416a9d3f6dc90274a72ca907743632
spoonty <sergey.kachalov.02@gmail.com>
2022-04-24 18:22:14 +0700

    Movement of snake with multiple cells

--- src/components/game/gameWindow/gameWindow.jsx
+++ src/components/game/gameWindow/gameWindow.jsx
@@ -3,16 +3,16 @@ import style from './gameWindow.module.css';
 import CharacterCell from "./characterCell/characterCell";
 
 const GameWindow = (props) => {
-    const [pos, setPos] = useState({
-        x: 0,
-        y: 0
-    });
+    let gameInterval;
 
-    const defineCoordinates = () => {
+    const [characters, setCharacters] = useState(Array);
+    const [lose, setLose] = useState(false);
+
+    const defineCoordinates = (direction) => {
         let x = 0;
         let y = 0;
 
-        switch (props.direction) {
+        switch (direction) {
             case 'up':
                 y = -1;
                 break;
@@ -30,33 +30,69 @@ const GameWindow = (props) => {
         return {x, y}
     }
 
+    const getAsh = () => {
+        return characters.filter(val => val.id === 0);
+    }
+
+    const characterMovement = (pos) => {
+        const direction = props.direction;
+
+        if ((getAsh()[0].x < 0 && getAsh()[0].current === 'left') ||
+            (getAsh()[0].x > 9 && getAsh()[0].current === 'right') ||
+            (getAsh()[0].y < 0 && getAsh()[0].current === 'up') ||
+            (getAsh()[0].y > 9 && getAsh()[0].current === 'down')) {
+            clearInterval(gameInterval);
+            return;
+        }
+
+        setCharacters(() => {
+            let nextCoordinates;
+            let current, prev = pos.current;
+
+            if (pos.id === 0) {
+                nextCoordinates = defineCoordinates(direction);
+                current = direction;
+            } else {
+                nextCoordinates = defineCoordinates(characters[pos.id - 1].prev);
+                current = characters[pos.id - 1].prev;
+            }
+
+            let array = characters.filter((val, index) => index !== pos.id);
+
+            return [
+                ...array,
+                characters[pos.id] = {
+                    ...characters[pos.id],
+                    x: characters[pos.id].x + nextCoordinates.x,
+                    y: characters[pos.id].y + nextCoordinates.y,
+                    prev: prev,
+                    current: current
+                }
+            ]
+        })
+    }
+
     useEffect(() => {
-        const interval = setInterval(() => {
+        setCharacters([{id: 0, x: 2, y: 0, current: props.direction, prev: props.direction},
+            {id: 1, x: 1, y: 0, prev: props.direction, current: props.direction},
+            {id: 2, x: 0, y: 0, prev: props.direction, current: props.direction}]);
+    }, []);
 
-            if ((pos.x > 0 || props.direction !== 'left') &&
-                (pos.x < 9 || props.direction !== 'right') &&
-                (pos.y > 0 || props.direction !== 'up') &&
-                (pos.y < 9 || props.direction !== 'down')) {
-                setPos(prevState => {
-                    const nextCoordinates = defineCoordinates();
+    useEffect(() => {
 
-                    return {
-                        x: prevState.x + nextCoordinates.x,
-                        y: prevState.y + nextCoordinates.y
-                    }
-                })
-            }
-            else {
-                clearInterval(interval);
-            }
+
+        gameInterval = setInterval(() => {
+            characters.map(character => characterMovement(character));
         }, 500)
 
-        return () => clearInterval(interval);
-    }, [pos]);
+        return () => clearInterval(gameInterval);
+    }, [characters]);
 
     return (
         <div className={style.gameWindow}>
-            <CharacterCell pos={pos}/>
+            {
+                characters.map(character => <CharacterCell key={character.id} pos={character} />)
+            }
         </div>
     );
 }
